3e9b1a90dc07493979cfea46688aa7496b1af8a4
arsal0308 <arsal.imran0308@gmail.com>
2023-08-30 12:01:35 -0400

    MERN-Stack Infrastructure

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+require('dotenv').config();
+require('./config/database');
+
+let user, item, category, order;
+let users, items, categories, orders;
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+	
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(require('./config/checkToken'));
+
+const port = process.env.PORT || 3001;
+app.use('/api/users', require('./routes/api/users'));
+
+// The following "catch all" route (note the *) is necessary
+// to return the index.html on all non-AJAX requests
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+  
+ app.listen(port, function() {
+   console.log(`Express app running on port ${port}`)
+ });
