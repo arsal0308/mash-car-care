8fbcca079f2cdd5c2809762f6d002acad2b2ff71
arsal0308 <arsal.imran0308@gmail.com>
2023-09-11 12:31:50 -0400

    updated styling pt 2

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,7 +1,33 @@
-export default function OrderHistoryPage() {
-    return (
-        <>
-            <h1>OrderHistoryPage</h1>
-        </>
-    );
+import React, { useState, useEffect } from 'react';
+import * as ordersAPI from "../../utilities/orders-api";
+import OrderDetail from '../../components/OrderDetail/OrderDetail';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
+
+export default function OrderHistoryPage({ user, setUser }) {
+  const [orderHistory, setOrderHistory] = useState([]);
+
+  useEffect(() => {
+    async function fetchOrderHistory() {
+      try {
+        const orders = await ordersAPI.getOrderHistory();
+        setOrderHistory(orders);
+      } catch (error) {
+        console.error('Error fetching order history:', error);
+      }
+    }
+
+    fetchOrderHistory();
+  }, []);
+
+  return (
+    <main className="OrderHistoryPage">
+      <header>
+        <h1>Order History</h1>
+      </header>
+      {orderHistory.map((order, index) => (
+        <OrderDetail key={index} order={order} />
+      ))}
+      <UserLogOut user={user} setUser={setUser} />
+    </main>
+  );
 }
