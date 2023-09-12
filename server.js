5ed95830a0ecc7e14ce6e9294976254804fbdb1b
arsal0308 <arsal.imran0308@gmail.com>
2023-09-12 09:56:08 -0400

    README updated

--- server.js
+++ server.js
@@ -6,18 +6,18 @@ const logger = require('morgan');
 require('dotenv').config();
 require('./config/database');
 
-let user, item, category, order;
-let users, items, categories, orders;
 
 const app = express();
+
+// const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
 
 app.use(logger('dev'));
 app.use(express.json());
 	
-// const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
 
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
+
 app.use(require('./config/checkToken'));
 
 const port = process.env.PORT || 3001;
@@ -27,8 +27,6 @@ const ensureLoggedIn = require('./config/ensureLoggedIn');
 app.use('/api/items', ensureLoggedIn, require('./routes/api/items'));
 app.use('/api/orders', ensureLoggedIn, require('./routes/api/orders'));
 
-// The following "catch all" route (note the *) is necessary
-// to return the index.html on all non-AJAX requests
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
