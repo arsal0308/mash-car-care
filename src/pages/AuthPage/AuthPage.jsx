63c6868d1787f8175934fd7da821e601988bfeb8
arsal0308 <arsal.imran0308@gmail.com>
2023-09-13 13:40:54 -0400

    Updated AuthPage Styling

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,12 +1,14 @@
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import LogInForm from "../../components/LogInForm/LogInForm";
-
+import './AuthPage.css';
+ 
 export default function AuthPage({ setUser, user }) {
     return (
         <main>
-                <SignUpForm setUser={setUser} />
-                <h3>or</h3>
-                <LogInForm setUser={setUser} />
+            <h1>Welcome to Mash Car Care</h1>
+            <SignUpForm setUser={setUser} />
+            <h3>or</h3>
+            <LogInForm setUser={setUser} />
         </main>
     );
 }
